5f8600c3edf8132ca94061d1fc841ba690876854
yama5993 <yama5993@nate.com>
2025-10-27 13:20:44 +0900

    추적 코드 수정: Google Tag Manager ID를 G-197LKDJGPJ로 변경

--- assets/js/ga.js
+++ assets/js/ga.js
@@ -1,7 +1,7 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-MTBCHM8DYM', {
+gtag('config', 'G-197LKDJGPJ', {
 	linker: {
 		domains: ['latindance.kr', 'archerlab.dev', 'itstory.archerlab.dev', 'walkwithme.archerlab.dev']
 	}
