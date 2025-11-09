a33bad951837e8a8e38a5120414c922dcdb3f59e
janghyungyu <yama5993@nate.com>
2025-11-09 21:42:24 +0900

    VENUES 정보 추가: 주크서울 정보 및 이미지 추가

--- assets/js/data/seoul.js
+++ assets/js/data/seoul.js
@@ -147,6 +147,33 @@ const SEOUL_VENUES = [
     ]
   },
   {
+    id: "seoul-zoukseoul",
+    name: { ko: "주크서울", en: "Zouk Seoul" },
+    region: { ko: "서울", en: "Seoul" },
+    city: { ko: "서울 마포구", en: "Mapo-gu" },
+    address: {
+      ko: "서울특별시 마포구",
+      en: "Mapo-gu, Seoul"
+    },
+    styles: ["zouk"],
+    summary: {
+      ko: "2016년 한국 최초로 시작된 주크 전문 모임으로, 브라질리언 주크를 보급하고 활성화하기 위해 정기 강습과 소셜 파티를 열며 주크 문화의 중심 역할을 합니다.",
+      en: "Founded in 2016 as Korea's first dedicated zouk community, it leads the promotion of Brazilian zouk through regular classes and social parties while anchoring the local zouk scene."
+    },
+    image: "assets/images/zouk_seoul.png",
+    imageAlt: {
+      ko: "주크서울 로고",
+      en: "Zouk Seoul logo"
+    },
+    links: [
+      { type: "cafe", url: "https://cafe.naver.com/zoukseoul" },
+      { type: "instagram", url: "https://www.instagram.com/zoukseoul/" },
+      { type: "blog", url: "https://m.blog.naver.com/zoukseoul" },
+      { type: "facebook", url: "https://www.facebook.com/people/zoukseoul/100080266793226/" },
+      { type: "youtube", url: "https://www.youtube.com/@ZoukSeoul" }
+    ]
+  },
+  {
     id: "seoul-sda",
     name: { ko: "SDA 살사댄스아카데미", en: "Salsa Dance Academy (SDA)" },
     region: { ko: "서울", en: "Seoul" },
