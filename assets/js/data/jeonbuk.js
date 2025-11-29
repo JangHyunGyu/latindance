d203bcf5643b6607295113c5248fe910dbec7b5d
janghyungyu <yama5993@nate.com>
2025-11-29 15:36:30 +0900

    feat: 전북 및 전남 지역 라틴댄스 클럽 데이터 추가

--- assets/js/data/jeonbuk.js
+++ assets/js/data/jeonbuk.js
@@ -1,44 +1,8 @@
-var JEOLLA_VENUES = [
-  {
-    id: "jeonnam-suncheon-latin",
-    name: { ko: "순천라틴댄스", en: "Suncheon Latin Dance", es: "Suncheon Latin Dance" },
-    region: { ko: "전라", en: "Jeolla", es: "Jeolla" },
-    city: { ko: "전남 순천시", en: "Suncheon", es: "Suncheon" },
-    address: {
-      ko: "전남 순천시 하대석길 35 B1",
-      en: "B1, 35 Hadaeseok-gil, Suncheon-si, Jeollanam-do",
-      es: "B1, 35 Hadaeseok-gil, Suncheon-si, Jeollanam-do"
-    },
-    styles: ["salsa", "bachata"],
-    summary: {
-      ko: "25년의 역사를 가진 라틴댄스의 명가이자 지역사회를 위한 무료 공연 활동도 활발한 라틴댄스 동호회입니다.",
-      en: "Latin dance community in Suncheon where you can enjoy salsa and bachata.",
-      es: "Comunidad de baile latino en Suncheon donde puedes disfrutar de salsa y bachata."
-    },
-    image: "assets/images/suncheon_latin.png",
-    imageAlt: {
-      ko: "순천라틴댄스 로고",
-      en: "Suncheon Latin Dance logo",
-      es: "Logotipo de Suncheon Latin Dance"
-    },
-    links: [
-      { type: "cafe", url: "https://cafe.naver.com/muybien2" },
-      { type: "instagram", url: "https://www.instagram.com/sunladan_salsa_bachata/" },
-      {
-        type: "phone",
-        url: "tel:010-5020-5907",
-        label: {
-          ko: "모조",
-          en: "Mojo",
-          es: "Mojo"
-        }
-      }
-    ]
-  },
+var JEONBUK_VENUES = [
   {
     id: "jeonbuk-jeonju-latin",
     name: { ko: "전주라틴댄스클럽", en: "Jeonju Latin Dance Club", es: "Jeonju Latin Dance Club" },
-    region: { ko: "전라", en: "Jeolla", es: "Jeolla" },
+    region: { ko: "전북", en: "Jeollabuk-do", es: "Jeollabuk-do" },
     city: { ko: "전북 전주시", en: "Jeonju", es: "Jeonju" },
     address: {
       ko: "전북 전주시 완산구 문화광장로 28 4층",
@@ -75,7 +39,7 @@ var JEOLLA_VENUES = [
   {
     id: "jeonbuk-latin-cruise",
     name: { ko: "라틴크루즈", en: "Latin Cruise", es: "Latin Cruise" },
-    region: { ko: "전라", en: "Jeolla", es: "Jeolla" },
+    region: { ko: "전북", en: "Jeollabuk-do", es: "Jeollabuk-do" },
     city: { ko: "전북 전주시", en: "Jeonju", es: "Jeonju" },
     address: {
       ko: "전북 전주시 덕진구 명륜4길 17-25 3층 바일란도빠",
---
+++ assets/js/data/jeonnam.js
@@ -0,0 +1,38 @@
+var JEONNAM_VENUES = [
+  {
+    id: "jeonnam-suncheon-latin",
+    name: { ko: "순천라틴댄스", en: "Suncheon Latin Dance", es: "Suncheon Latin Dance" },
+    region: { ko: "전남", en: "Jeollanam-do", es: "Jeollanam-do" },
+    city: { ko: "전남 순천시", en: "Suncheon", es: "Suncheon" },
+    address: {
+      ko: "전남 순천시 하대석길 35 B1",
+      en: "B1, 35 Hadaeseok-gil, Suncheon-si, Jeollanam-do",
+      es: "B1, 35 Hadaeseok-gil, Suncheon-si, Jeollanam-do"
+    },
+    styles: ["salsa", "bachata"],
+    summary: {
+      ko: "25년의 역사를 가진 라틴댄스의 명가이자 지역사회를 위한 무료 공연 활동도 활발한 라틴댄스 동호회입니다.",
+      en: "Latin dance community in Suncheon where you can enjoy salsa and bachata.",
+      es: "Comunidad de baile latino en Suncheon donde puedes disfrutar de salsa y bachata."
+    },
+    image: "assets/images/suncheon_latin.png",
+    imageAlt: {
+      ko: "순천라틴댄스 로고",
+      en: "Suncheon Latin Dance logo",
+      es: "Logotipo de Suncheon Latin Dance"
+    },
+    links: [
+      { type: "cafe", url: "https://cafe.naver.com/muybien2" },
+      { type: "instagram", url: "https://www.instagram.com/sunladan_salsa_bachata/" },
+      {
+        type: "phone",
+        url: "tel:010-5020-5907",
+        label: {
+          ko: "모조",
+          en: "Mojo",
+          es: "Mojo"
+        }
+      }
+    ]
+  }
+];
