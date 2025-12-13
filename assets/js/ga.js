891b185b57e8559a7cbbdcfc94de35dcc0f972bb
janghyungyu <yama5993@nate.com>
2025-12-13 12:56:50 +0900

    fix: GA 설정에 golf.archerlab.dev 도메인 추가

--- assets/js/ga.js
+++ assets/js/ga.js
@@ -3,6 +3,6 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-197LKDJGPJ', {
 	linker: {
-		domains: ['latindance.kr', 'archerlab.dev', 'itstory.archerlab.dev', 'walkwithme.archerlab.dev']
+		domains: ['latindance.kr', 'archerlab.dev', 'itstory.archerlab.dev', 'walkwithme.archerlab.dev', 'golf.archerlab.dev']
 	}
 });
